3ccb8ca302fdc804f1b07e555dce9533f1bdeb8a
crux26 <crux26@business.kaist.ac.kr>
2017-11-01 19:17:53 +0900

    Split the option data into near-/next-term. All others are discarded.

--- split2nearNnext.m
+++ split2nearNnext.m
@@ -1,28 +1,45 @@
 function [Kc_1st, C_1st, Kp_1st, P_1st, DTM_1st, ...
     Kc_2nd, C_2nd, Kp_2nd, P_2nd, DTM_2nd, isSTD_1st, isSTD_2nd, today_] = ...
     split2nearNnext(CallData, PutData)
+% If XData.exdate.length==2, split them into near- and next-term.
+% If Xdata.exdate.length==1, deal the same data into near- and next-term.
+% Result will be used for the interpolation, so nothing wrong with this method.
 
-tmp_Expiries_C = unique( CallData(:, 2) );  % size must be 2.
-if length(tmp_Expiries_C) ~= 2
-    error('There is maturity other than near- and next-term.');
+% Length can only be either 1 or 2.
+tmp_Expiries_C = unique( CallData(:, 2) );  % length()==2 in general: Guaranteed by idNear30D_wrapper().
+if length(tmp_Expiries_C) == 1
+    tmp_Expiries_C = ones(2,1) * tmp_Expiries_C;
+elseif length(tmp_Expiries_C) > 3
+    error('tmp_Expiries_C.length() > 3.');
 end
 tmpIdx_C_1st = find( CallData(:, 2) == tmp_Expiries_C(1) ); % Shorter DTM.
 tmpIdx_C_2nd = find( CallData(:, 2) == tmp_Expiries_C(2) ); % Longer DTM.
 
-tmp_Expiries_P = unique( PutData(:, 2) );   % size must be 2.
-if length(tmp_Expiries_P) ~= 2
-    error('There is maturity other than near- and next-term.');
+
+%%
+% Length can only be either 1 or 2.
+tmp_Expiries_P = unique( PutData(:, 2) );   % length()==2 in general.
+if length(tmp_Expiries_P) == 1
+    tmp_Expiries_P = ones(2,1) * tmp_Expiries_P;
+elseif length(tmp_Expiries_P) > 3
+    error('tmp_Expiries_P.length() > 3.');
 end
 tmpIdx_P_1st = find( PutData(:, 2) == tmp_Expiries_P(1) );  % Shorter DTM.
 tmpIdx_P_2nd = find( PutData(:, 2) == tmp_Expiries_P(2) );  % Longer DTM.
 
-if tmp_Expiries_C(1) ~= tmp_Expiries_P(1)
-    error('1st month maturities of call and put does not match.');
+%%
+% Sanity check
+if length(tmp_Expiries_C)==2 && length(tmp_Expiries_P)==2
+    if tmp_Expiries_C(1) ~= tmp_Expiries_P(1)
+        error('1st month maturities of call and put does not match.');
+    end
+    if tmp_Expiries_C(2) ~= tmp_Expiries_P(2)
+        error('2nd month maturities of call and put does not match.');
+    end
+else
+    error('Inconsistency between tmp_Expiries_C and tmp_Expiries_P.');
 end
-if tmp_Expiries_C(2) ~= tmp_Expiries_P(2)
-    error('2nd month maturities of call and put does not match.');
-end
-
+    
 Kc_1st = CallData(tmpIdx_C_1st, 3); % CallData(:,3): call's strike price (Kc)
 Kc_2nd = CallData(tmpIdx_C_2nd, 3);
 
