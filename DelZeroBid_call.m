2461392178da839825ae4516d156aa00f7023a55
crux26 <crux26@business.kaist.ac.kr>
2017-11-04 21:45:40 +0900

    Files moved.

--- DelZeroBid_call.m
+++
@@ -1,5 +0,0 @@
-function [Kc, C_bid, C_ask] = DelZeroBid_call(Kc, C_bid, C_ask)
-idx_nonZero = find(C_bid ~= 0);
-Kc = Kc(idx_nonZero);
-C_bid = C_bid(idx_nonZero);
-C_ask = C_ask(idx_nonZero);
--- DelZeroBid_put.m
+++
@@ -1,6 +0,0 @@
-function [Kp, P_bid, P_ask] = DelZeroBid_put(Kp, P_bid, P_ask)
-idx_nonZero = find(P_bid ~= 0);
-Kp = Kp(idx_nonZero);
-P_bid = P_bid(idx_nonZero);
-P_ask = P_ask(idx_nonZero);
-
